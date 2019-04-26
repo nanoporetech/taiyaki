3bdbc4feba4db49af8095e655389cebe79e5aa5e
Tim Massingham <tim.massingham@nanoporetech.com>
2019-04-26 16:50:58 +0100

    Modified base walk-through

--- taiyaki/__init__.py
+++ taiyaki/__init__.py
@@ -1,7 +1,7 @@
 """Custard owns my heart!"""
 __version_info__ = {
-    'major': 3,
-    'minor': 1,
+    'major': 4,
+    'minor': 0,
     'revision': 0,
 }
 __version__ = "{major}.{minor}.{revision}".format(**__version_info__)
