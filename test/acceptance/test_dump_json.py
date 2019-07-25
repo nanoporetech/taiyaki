bc3ab2a2a26ae52ff9e05f165a43d2aa128ddf50
Marcus Stoiber <marcus.stoiber@nanoporetech.com>
2019-07-25 17:51:18 +0100

    Edits to make json_to_checkpoint test work.

--- test/acceptance/test_dump_json.py
+++ test/acceptance/test_dump_json.py
@@ -71,23 +71,36 @@ class AcceptanceTest(unittest.TestCase):
         self.assertTrue(os.path.exists(self.model_file))
         test_work_dir = self.work_dir(os.path.join("test_json_to_checkpoint", subdir))
 
-        output_file = os.path.join(test_work_dir, "output.json")
-        cmd = [self.script, self.model_file, "--output", output_file, "--params"]
+        # dump stored model to json
+        json_file = os.path.join(test_work_dir, "output.json")
+        # open and remove file in case it remains from previous test
+        open(json_file, "w").close()
+        os.remove(json_file)
+        cmd = [self.script, self.model_file, "--output", json_file, "--params"]
         util.run_cmd(self, cmd).expect_exit_code(0)
-        self.assertTrue(os.path.exists(output_file))
+        self.assertTrue(os.path.exists(json_file))
 
+        # convert json back to checkpoint
         re_model_file = os.path.join(test_work_dir, "re_model.checkpoint")
-        cmd = [self.json_to_cp, output_file, "--output", re_model_file]
-
+        cmd = [self.json_to_cp, json_file, "--output", re_model_file]
         open(re_model_file, "w").close()
         error_message = "RuntimeError: File/path for 'output' exists, {}".format(re_model_file)
         util.run_cmd(self, cmd).expect_exit_code(1).expect_stderr(util.any_line_starts_with(error_message))
         os.remove(re_model_file)
-
         util.run_cmd(self, cmd).expect_exit_code(0)
-
         self.assertTrue(os.path.exists(re_model_file))
-        model_dump = open(self.model_file, 'r').read()
-        re_model_dump = open(re_model_file, 'r').read()
 
-        self.assertEqual(model_dump, re_model_dump)
+        # and finally convert checkpoint back to json again for comparison
+        json_file2 = os.path.join(test_work_dir, "output2.json")
+        # open and remove file in case it remains from previous test
+        open(json_file2, "w").close()
+        os.remove(json_file2)
+        cmd = [self.script, re_model_file, "--output", json_file2, "--params"]
+        util.run_cmd(self, cmd).expect_exit_code(0)
+        self.assertTrue(os.path.exists(json_file2))
+
+        # note that checkpoints are not equal with a binary dump, but the
+        # json files are due to the unused GRU bias parameters.
+        json_dump = open(json_file, 'r').read()
+        json_dump2 = open(json_file2, 'r').read()
+        self.assertEqual(json_dump, json_dump2)
