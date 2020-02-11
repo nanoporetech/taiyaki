5c529a4f2a639031bca318461a8642a54b11abad
Marcus Stoiber <marcus.stoiber@nanoporetech.com>
2020-02-11 12:42:18 +0000

    Remove chunk_selection.assmble_batch function

--- taiyaki/chunk_selection.py
+++ taiyaki/chunk_selection.py
@@ -77,35 +77,37 @@ def sample_chunks(read_data, number_to_sample, chunk_len, filter_params,
                   standardize=True, select_strands_randomly=True,
                   first_strand_index=0):
     """Sample <number_to_sample> chunks from a list of read_data, returning
-    a tuple (chunklist, rejection_dict)
+    a tuple (chunklist, rejection_dict).
+
+    Random read sampling continues until <number_to_sample> chunks that pass
+    filter_params have been sampled or <fraction_of_fails_allowed> has been
+    reached.
 
     rejection_dict is a dictionary with keys describing the reasons for
     rejection and values being the number rejected for that reason. E.g.
     {'pass':3,'meandwell':3, 'maxdwell':4}.
 
-    param: read_data        : a list of Read objects as defined in mapped_signal_files.py
-    param: number_to_sample : target number of data elements to return, each from
-                              a sampled chunk. If number_to_sample is 0 or None
-                              then get the same number of chunks as the number
-                              of read_data items supplied.
-    param: chunk_len        : desired length of chunk in samples, or length
-                              of sequence in bases if chunk_len_means_sequence_len
-    param: fraction_of_fails_allowed : Visit a maximum of
-                             (number_to_sample / fraction_of_fails_allowed) reads
-                             before stopping.
-    param: filter_params    : taiyaki.chunk_selection.FILTER_PARAMETERS namedtuple
-    param: chunk_len_means_sequence_len : if this is False (the default) then
-                             chunk_len determines the length in samples of the
-                             chunk, and we use mapped_signal_files.get_chunk_with_sample_length().
-                             If this is True, then chunk_len determines the length
-                             in bases of the sequence in the chunk, and we use
-                             mapped_signal_files.get_chunk_with_sequence_length()
-    :param standardize:      Return standardized currents, otherwise unscaled
+    param: read_data: a list of Read objects as defined in
+        mapped_signal_files.py
+    param: number_to_sample: target number of data elements to return, each from
+        a sampled chunk. If number_to_sample is 0 or None then get the same
+        number of chunks as the number of read_data items supplied.
+    param: chunk_len: desired length of chunk in samples, or length of sequence
+        in bases if chunk_len_means_sequence_len
+    param: fraction_of_fails_allowed: Visit a maximum of
+        (number_to_sample / fraction_of_fails_allowed) reads before stopping.
+    param: filter_params: taiyaki.chunk_selection.FILTER_PARAMETERS namedtuple
+    param: chunk_len_means_sequence_len: if this is False (the default) then
+        chunk_len determines the length in samples of the chunk, and we use
+        mapped_signal_files.get_chunk_with_sample_length(). If this is True,
+        then chunk_len determines the length in bases of the sequence in the
+        chunk, and we use mapped_signal_files.get_chunk_with_sequence_length()
+    :param standardize: Return standardized currents, otherwise unscaled
     param: select_strands_randomly : Choose a random read at each iteration.
-                             When select_strands_randomly=False, iterate
-                             through reads as found in read_data.
+        When select_strands_randomly=False, iterate through reads as found
+        in read_data.
     param: first_strand_index : When select_strands_randomly=False, begin
-                             selecting strands at this index.
+        selecting strands at this index.
     """
     nreads = len(read_data)
     if number_to_sample is None or number_to_sample == 0:
@@ -153,32 +155,3 @@ def sample_filter_parameters(read_data, number_to_sample, chunk_len,
     return FILTER_PARAMETERS(
         filter_mean_dwell=filter_mean_dwell, filter_max_dwell=filter_max_dwell,
         median_meandwell=median_meandwell, mad_meandwell=mad_meandwell)
-
-
-def assemble_batch(read_data, batch_size, chunk_len, filter_params,
-                   chunk_len_means_sequence_len=False, standardize=True,
-                   select_strands_randomly=True, first_strand_index=0):
-    """Assemble a batch of data by repeatedly choosing a random (or iterating
-    over reads) read and location in that read, continuing until we have found
-    batch_size chunks that pass the tests.
-
-    Returns tuple (chunklist, rejection_dict)
-
-    where chunklist is a list of dicts, each with entries
-        (signal_chunk, sequence_chunk, start_sample, read_id).
-        signal_chunks and sequence_chunks are np arrays.
-    and rejection_dict is a dictionary with keys describing the reasons for
-        rejection and values being the number rejected for that reason. E.g.
-        {'pass':3,'meandwell':3, 'maxdwell':4}.
-
-    If we can't find enough chunks after the allowed number of attempts ,then
-    return the short batch, but output a message to the log.
-
-    See docstring for sample_chunks for parameters.
-    """
-    return sample_chunks(read_data, batch_size, chunk_len,
-                         filter_params=filter_params,
-                         chunk_len_means_sequence_len=chunk_len_means_sequence_len,
-                         standardize=standardize,
-                         select_strands_randomly=select_strands_randomly,
-                         first_strand_index=first_strand_index)
