22b1e46901a7c1326b3d250ed311417c409af02b
Tim Massingham <tim.massingham@nanoporetech.com>
2020-11-13 14:22:07 +0000

    Bump to 5.3

--- taiyaki/__init__.py
+++ taiyaki/__init__.py
@@ -1,7 +1,7 @@
 """Custard owns my heart!"""
 __version_info__ = {
     'major': 5,
-    'minor': 1,
+    'minor': 3,
     'revision': 0,
 }
 __version__ = "{major}.{minor}.{revision}".format(**__version_info__)
